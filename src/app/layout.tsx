b64a9ddecca5d23b6efae02dd09821cc9ec3efdd
Amit Masram <amitmasram831@gmail.com>
2026-06-29 23:49:14 +0530

    fix: update favicon to new AmitIcon asset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import AmitIcon from '@/assets/alogo1.ico'
+import AmitIcon from '@/assets/alogo3.ico'
 import { Inter } from 'next/font/google'
 import { Analytics } from "@vercel/analytics/next"
 
